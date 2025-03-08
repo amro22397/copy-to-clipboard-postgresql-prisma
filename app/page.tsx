e9bef0a7552946a8c1adf307ce4fb4143de18094
amro22397 <amroalmutasim22@gmail.com>
2025-03-08 09:11:24 +0400

    connectToDatabase

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,13 @@ import ThePage from "./thePage";
 import textArea from "@/models/text-area";
 import text from "@/models/text";
 
+import { connectToDatabase } from '@/lib/db'
+
 const page = async () => {
 
-  mongoose.connect(process.env.MONGO_URL as string);
+  // mongoose.connect(process.env.MONGO_URL as string);
+  connectToDatabase();
+  
   const textsData = await text.find({});
   const jTextsData = JSON.parse(JSON.stringify(textsData));
 

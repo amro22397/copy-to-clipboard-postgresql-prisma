9dde11b9f21cfa877ecea22e3b779419fa62b8f8
amro22397 <amroalmutasim22@gmail.com>
2025-03-09 11:02:01 +0400

    mongoose connect 2

--- pages/api/send-forget-email.ts
+++ pages/api/send-forget-email.ts
@@ -4,6 +4,7 @@ import VerifyEmailTemplate from "@/app/emails/VerifyEmailTemplate";
 import { User } from "@/models/user";
 import crypto from 'crypto'
 import { connectToDatabase } from "@/lib/db";
+import mongoose from "mongoose";
 
 
 
@@ -34,7 +35,7 @@ export default async function handler(req: Request, res: Response) {
     // })
   }
 
-
+  mongoose.connect(process.env.MONGO_URL as string);
   const user = await User.findOne({ email: email })
 
 

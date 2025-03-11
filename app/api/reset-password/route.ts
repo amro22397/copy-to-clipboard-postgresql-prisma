65b8a04c582b3106d51c60fbbc44374f6f4d0042
amro22397 <amroalmutasim22@gmail.com>
2025-03-11 08:53:37 +0400

    fix reset password

--- app/api/reset-password/route.ts
+++ app/api/reset-password/route.ts
@@ -1,46 +1,51 @@
-import mongoose from "mongoose";
-import crypto from "crypto"
 import { User } from "@/models/user";
+import bcrypt from "bcrypt"
+import mongoose from "mongoose";
+import crypto from 'crypto'
 
-export async function PUT(req: Request) {
+
+export async function POST(req: Request) {
     mongoose.connect(process.env.MONGO_URL as string);
-    const { verificationToken, userId } = await req.json();
+    const { token, password } = await req.json();
+
+    const hashedToken = crypto.createHash("sha256").update(token).digest("hex");
 
     try {
-        const verifyToken = crypto.createHash("sha256").update(verificationToken).digest("hex");
-        console.log(verifyToken)
-        
+        const user = await User.findOne({ 
+            resetPasswordToken: hashedToken,
+            resetPasswordExpires: { $gt: Date.now() },
+         });
 
-    const user = await User.findOne({
-        _id: userId,
-        verifyToken: verifyToken,
-        verifyTokenExpires: { $gt: new Date()},
-    });
+         if (!user) {
+            return Response.json({
+                message: "Invalid token or has expired",
+                success: false,
+            })
+         }
 
-    if (!user) {
-        return Response.json({
-            message: "Invalid token or has expired",
-            success: false,
-        })
-    }
-    
-    await User.updateOne({ email: user.email }, { $set: {
-        isVerified: true,
-        verifyToken: null,
-        verifyTokenExpires: null,
-    }});
+         const hashedPassword = await bcrypt.hash(password, 12);
 
-    return Response.json({
-        message: 'Email is verified',
-        success: true,
-    });
+         await User.updateOne({ email: user.email }, { $set: {
+            resetPasswordToken: null,
+            resetPasswordExpires: null,
+            hashedPassword: hashedPassword,
+         }})
+
+         return Response.json({
+            message: "Password was reset successfully",
+               success: true,
+         })
 
     } catch (error) {
-        console.log(error);
-        return Response.json({
-            message: `${error}`,
+
+      console.log(error);
+      return Response.json({
+         message: `There is an error: ${error}`,
             success: false,
-        })
+      })
+
     }
+
+
 
 }
